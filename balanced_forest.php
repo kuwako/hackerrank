6dce0d325c1e182554c9327db63eca59989d8390
masaki_kuwako <m.kuwako0702@gmail.com>
2018-08-29 23:45:18 +0900

    溶けなかった

--- balanced_forest.php
+++ balanced_forest.php
@@ -2,14 +2,23 @@
 
 // Complete the balancedForest function below.
 function balancedForest($c, $edges) {
-
   // $c = [0, 1, 1, 2, 3];
   // $edges = [[1,2], [1,3], [1,4], [4,5]]
-  var_dump($c);
-  var_dump($edges);
-
-  // dfsをやるのかなぁ...
+  // 木構造を作ってやる...?
   // 動的計画法的なやり方で枝の計算を事前にやる...?
+  $tree = [];
+  $tree[1][1] = $c[0];
+  $edgeCnt = count($edges);
+  for ($i = 0; $i < $edgeCnt; $i++) {
+    $e = $edges[$i];
+    $l = $e[0];
+    $r = $e[1];
+    $tree[$l][$r] = $c[$r - 1];
+  }
+
+  // お手上げ
+  // こんな感じらしい(C#でも実行時間間に合わないらしい)
+  // http://yambe2002.hatenablog.com/entry/2016/07/25/102636
 }
 
 $fptr = fopen(getenv("OUTPUT_PATH"), "w");
